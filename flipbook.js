7e950d4d948081cae53f70ca552d5fb0dbde835f
Johnnie Munger <johnnietheblack@gmail.com>
2013-08-15 12:23:26 -0700

    added new version of js

--- flipbook.js
+++ flipbook.js
@@ -17,15 +17,20 @@
 		var vars = {
 			backgroundPosition	: 'center',
 			backgroundSize		: 'cover',
+			coastDegradeValue	: 0.98,
+			coastTimer			: null,
 			currentFrame		: 0,
 			currentNotch		: null,
+			currentVelocity		: 0,
 			currentX			: null,
 			isMobile 			: (function(a){ return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|ipad|iemobile|ip(ad|hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera),
 			isPlaying			: false,
+			motionTimerStart	: null,
 			notchInterval		: 5,
 			playTimer			: null,
 			prevFrame			: null,
-			prevX				: null
+			prevX				: null,
+			velocities			: [],
 		};
 		
 		var $parent = $(el);
@@ -33,25 +38,24 @@
 		var analyzeMovement = function(x) {
 			vars.prevX = vars.currentX;
 			vars.currentX = x;
+			
+			
+			stopCoast();
+			detectCurrDirection();
 
 			// if first movement
 			if(!vars.currentNotch) {
 				vars.currentNotch = vars.currentX;
-
 			}else{
-
-				// if moving right
-				if(vars.currentX > vars.prevX) {
-					if(vars.currentX >= (vars.currentNotch+vars.notchInterval)) {
-						vars.currentNotch = vars.currentX;
-						showNextImage();
-					}
-
-				// if moving left	
-				}else{
+				if(vars.currentDirection == 'left') {
 					if(vars.currentX <= (vars.currentNotch-vars.notchInterval)) {
 						vars.currentNotch = vars.currentX;
 						showPrevImage();
+					}
+				}else{
+					if(vars.currentX >= (vars.currentNotch+vars.notchInterval)) {
+						vars.currentNotch = vars.currentX;
+						showNextImage();
 					}
 				}
 			}
@@ -76,6 +80,46 @@
 			}
 		}
 		
+		var coast = function() {
+			stopCoast();
+			resetVelocities();
+			vars.currentVelocity = vars.currentVelocity*vars.coastDegradeValue;
+			var t = vars.notchInterval / vars.currentVelocity;
+			if(t < 200) {
+				vars.coastTimer = setTimeout(function() {
+					if(vars.currentDirection == 'left') {
+						showPrevImage();
+					}else{
+						showNextImage();
+					}
+				},t);
+			}
+		}
+		
+		var detectCurrDirection = function() {
+			if(vars.currentX > vars.prevX) {
+				vars.currentDirection = 'right';	
+			}else{
+				vars.currentDirection = 'left';
+			}
+		}
+		
+		var detectVelocity = function() {
+			var currTime = new Date().getTime();
+			if(vars.motionTimerStart > 0) {
+				var timeChange = currTime - vars.motionTimerStart;
+				vars.velocities.push(vars.notchInterval / timeChange);
+				
+				var avg = 0;
+				$.each(vars.velocities,function() {
+					avg += this;
+				});
+				avg = avg / vars.velocities.length;
+				vars.currentVelocity = avg;
+			}
+			vars.motionTimerStart = currTime;
+		}
+		
 		var playFlipbook = function() {
 			vars.isPlaying = true;
 			var interval = 1000/settings.frameRate;
@@ -88,6 +132,24 @@
 			var imgs = imgs.slice(0);
 			
 			$(imgs).each(function(i,v) {
+				
+				// append
+				var div = document.createElement("div");
+				div.className = "flipbook-frame flipbook-frame-"+i;
+				div.style.width = "100%";
+				div.style.height = "100%";
+				div.style.position = "absolute";
+
+				div.style.backgroundImage = "url('"+v+"')";
+				div.style.backgroundPosition = vars.backgroundPosition;
+				div.style.backgroundRepeat = "no-repeat";
+				div.style.backgroundSize = vars.backgroundSize;
+
+				div.style.display = "none";
+
+				$parent.append(div);
+				
+				// preload
 				$('<img>').attr({ src: this }).load(function() {
 
 					settings.images[i] = {
@@ -98,30 +160,22 @@
 
 					imgs.splice(imgs.indexOf(v),1);
 
-					var div = document.createElement("div");
-					div.className = "flipbook-frame flipbook-frame-"+i;
-					div.style.width = "100%";
-					div.style.height = "100%";
-					div.style.position = "absolute";
-
-					div.style.backgroundImage = "url('"+this.src+"')";
-					div.style.backgroundPosition = vars.backgroundPosition;
-					div.style.backgroundRepeat = "no-repeat";
-					div.style.backgroundSize = vars.backgroundSize;
-
-					div.style.display = "none";
-
-					$parent.append(div);
-
 					if(imgs.length == 0) { callback();}
 				});
 			});
+		}
+		
+		var resetVelocities = function() {
+			vars.velocities = [];
 		}
 		
 		var setup = function() {
 			
 			// if no images, bail
 			if(settings.images.length == 0) { return;}
+			
+			// setup sensitivity
+			vars.notchInterval = 100-settings.sensitivity;
 		
 			// pick wallpaper mode
 			switch(settings.wallpaperMode) {
@@ -166,12 +220,14 @@
 				
 				// on hover for desktop
 				}else{
-					$('body').on("mouseover", $parent, function() {
-				        bindMouse();
-				    });
-					$('body').on("mouseout", $parent, function() {
-				        unBindMouse();
-				    });
+					$parent.hover(
+						function() {
+							bindMouse();
+						},
+						function() {
+							unBindMouse();
+						}
+					);
 				}
 			});
 		};
@@ -180,13 +236,14 @@
 			
 			if(!i) { i=0;}
 
-			if(vars.prevFrame) {
-				$parent.children('.flipbook-frame-'+vars.prevFrame).hide();
+			if(!vars.prevFrame) { vars.prevFrame = 0;}
+			$parent.children('.flipbook-frame-'+vars.prevFrame).hide();
+			$parent.children('.flipbook-frame-'+i).show();
+			
+			if(settings.coast) {
+				detectVelocity();
+				coast();
 			}
-
-			$parent.children('.flipbook-frame-'+i).css({
-				'display':'block'
-			});
 		}
 		
 		var showNextImage = function() {
@@ -205,6 +262,10 @@
 			showImage(vars.currentFrame);
 		}
 
+		var stopCoast = function() {
+			clearTimeout(vars.coastTimer);
+		}
+		
 		var stopFlipbook = function() {
 			vars.isPlaying = false;
 			clearInterval(vars.playTimer);
@@ -244,11 +305,13 @@
 		'autoplay'				: false,
 		'backgroundColor'		: 'transparent',
 		'backgroundPosition'	: 'center',
+		'coast'					: false,
 		'fadeFrames'			: false,
 		'fadeFramesRate'		: null,
 		'frameRate'				: 24,
 		'images'				: {},
 		'resizeContainerToImage': false,
+		'sensitivity'			: 95,
 		'wallpaperMode'			: 'contain'
     };
 	
